bb4af2694ecf5d05f4cd661ad6bc3ac19f12f0c0
shareefa-rn <147628835+shareefa-rn@users.noreply.github.com>
2023-12-08 12:36:48 +0000

    Initial commit for payment test APP

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import StripeApp from "./src/StripeApp";
+import { StripeProvider } from "@stripe/stripe-react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <StripeProvider publishableKey="pk_test_51OKOMZDIjkIdotnugn0n3oJUqBl1UzYz4vPJAgTtyaRo7zGF5yv8xlnRFVX7tZP9fEykGNCYnskoSNG0A2wQoUW000ixXXUR6K">
+      <StripeApp />
+    </StripeProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ server/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import Stripe from "stripe";
+
+const app = express();
+const port = 3000; //add your port here
+
+const publishableKey =
+  "pk_test_51OKOMZDIjkIdotnugn0n3oJUqBl1UzYz4vPJAgTtyaRo7zGF5yv8xlnRFVX7tZP9fEykGNCYnskoSNG0A2wQoUW000ixXXUR6K";
+const secretKey =
+  "sk_test_51OKOMZDIjkIdotnuY5bs1WLUBjznyqGw3GbKO8X4QKft4FK6LxaaM22OImSeRkhfTfpjHXIKisHGi25rsS6Jtyl0008LR1QBMQ";
+
+//Confirm the API version from your stripe dashboard
+const stripe = Stripe(secretKey, { apiVersion: "2023-10-16" });
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+app.post("/create-payment-intent", async (req, res) => {
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: 1099, //lowest denomination of particular currency
+      currency: "usd",
+      payment_method_types: ["card"], //by default
+    });
+
+    const clientSecret = paymentIntent.client_secret;
+
+    res.json({
+      clientSecret: clientSecret,
+    });
+  } catch (e) {
+    console.log(e.message);
+    res.json({ error: e.message });
+  }
+});
